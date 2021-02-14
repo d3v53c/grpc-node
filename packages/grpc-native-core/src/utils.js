c92a346ad47fa4b71de0a359b6fcad1781525362
d3v53c <shitfi63@gmail.com>
2021-02-14 04:40:12 -0800

    disallow protopath overwrite

---
+++ packages/grpc-native-core/src/utils.js
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ * ILLEGAL KEYS 
+ */
+var ILLEGAL_KEYS = ['__proto__', 'prototype', 'constructor'];
+
+/**
+ * Returns true, if given key is included in the blacklisted
+ * keys.
+ * @param key key for check, string.
+ */
+exports.isIllegalKey = function (key) {
+  return ILLEGAL_KEYS.indexOf(key) !== -1;
+}
+
+/**
+ * Returns true if path is/has unsafe keys.
+ * @param path 
+ */
+exports.isProtoPath = function (path) {
+  return Array.isArray(path)
+    ? path.some(module.exports.isIllegalKey)
+    : typeof path === "string"
+      ? module.exports.isIllegalKey(path)
+      : false;
+}
+
+/**
+ * Disallow prototype path override
+ */
+exports.disallowProtoPath = function (path) {
+  if (module.exports.isProtoPath(path)) {
+    throw new Error('Unsafe path encountered: ' + path)
+  }
+}
